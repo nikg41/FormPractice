0f96113ad42e6a15292c54cb71cd6b3e09e469f9
Nikhil Garg <nikhil.garg@prudential.com.sg>
2022-01-17 15:52:41 +0530

    State code implemented

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [name, setName] = useState('');
+
+  const nameChangeHandler = event => {
+    setName(event.target.value);
+  };
+
+  const onSubmitHandler = event => {
+    event.preventDefault();
+
+    console.log(name);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setName('')
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          value={name}
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
